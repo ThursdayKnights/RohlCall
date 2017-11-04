7d91e3dc5818bf7a079e1fcfffe505865b065acf
VautourG <33331316+VautourG@users.noreply.github.com>
2017-11-04 11:44:49 -0300

    Made class
    Did math for stat mods.

---
+++ RohlCall/TKRCCharacterSheet.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RohlCall
+{
+    class TKRCCharacterSheet
+    {
+        private int ModMath(int stats)
+        {
+            int mod;
+            mod = (stats - 10) / 2;
+            return mod;
+        }
+    }
+   
+}
